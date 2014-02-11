3113effd3c03b5cde771cfb632470fbca81baf58
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-11 16:14:38 -0500

    Created template and wrote initial test for add function. 1 test failing.

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,7 +6,13 @@
     describe('In our todo app', function () {
         describe('when the add button is clicked', function () {
             it('should add a new todo div when the input is populated', function () {
+            	$('.js-new-todo-input').val('Get a ride to airport.');
 
+            	$('.js-add-todo').click();
+
+            	var fisrtTodoText = $('.todo-items').first().child('.description').text();
+
+            	expect(firstTodoText).to.contain('Get a ride to airport.');
             });
         });
     });
