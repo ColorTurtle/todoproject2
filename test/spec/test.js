d5eced2c26f82ce76b62ea2577b954966bb91518
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-11 15:26:27 -0500

    Initial commit.

---
+++ test/spec/test.js
@@ -0,0 +1,13 @@
+/* global describe, it */
+
+(function () {
+    'use strict';
+
+    describe('In our todo app', function () {
+        describe('when the add button is clicked', function () {
+            it('should add a new todo div when the input is populated', function () {
+
+            });
+        });
+    });
+})();
