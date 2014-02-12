95136c053348ef9fe6b6b52f1829e62fc798e558
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-11 19:10:29 -0500

    Corrected erros on main.js. Array items now populating. Minor changes to css.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,53 @@
-console.log('\'Allo \'Allo!');
+console.log('You can do this!');
+
+var todoList = [
+{
+	description: 'Pack for Texas',
+	done: false,
+	id: _.uniqueId('todo')
+}, 
+
+{
+	description: 'Drink some wine',
+	done: false,
+	id: _.uniqueId('todo')
+}, 
+
+{
+	description: 'Get a ride to the airport',
+	done: false,
+	id: _.uniqueId('todo')
+}
+
+];
+
+// Template from index
+var todoTemplate = _.template($('.todo-template').text())
+
+_.each(todoList, function(item){
+	$('.todo-items').prepend(todoTemplate(item));
+})
+
+$('.js-add-todo').click(function(){
+
+// pull the data from the user's input
+	var description = $('.js-new-todo-input').val();
+
+// push the user's input into an object
+var todo = {
+	description: description,
+	done: false,
+	id: _.uniqueId('todo')
+}
+
+// Push the user's data into a template using our template function
+var renderedTemplate = todoTemplate(todo);
+
+$('.todo-items').prepend(renderedTemplate);
+
+(todoList).push(todo)
+
+// $('.todo-input-box').trigger("reset");  
+// Not resetting - do I need to add a form tag for reset to work properly?
+
+});
--- test/spec/test.js
+++ test/spec/test.js
@@ -6,13 +6,15 @@
     describe('In our todo app', function () {
         describe('when the add button is clicked', function () {
             it('should add a new todo div when the input is populated', function () {
+            	
             	$('.js-new-todo-input').val('Get a ride to airport.');
 
             	$('.js-add-todo').click();
 
-            	var fisrtTodoText = $('.todo-items').first().child('.description').text();
+            	var firstTodoText = $('.todo-items').find($('.todo-description')).text();
 
             	expect(firstTodoText).to.contain('Get a ride to airport.');
+            	
             });
         });
     });
