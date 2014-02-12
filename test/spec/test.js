79818ddf6228c30b464e48d5f178e3449dca49ee
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-12 11:51:07 -0500

    Attempting to get test to pass.

--- test/spec/test.js
+++ test/spec/test.js
@@ -7,15 +7,19 @@
         describe('when the add button is clicked', function () {
             it('should add a new todo div when the input is populated', function () {
             	
-            	$('.js-new-todo-input').val('Get a ride to airport.');
+            	$('.js-new-todo-input').val('Grab some beer.');
 
             	$('.js-add-todo').click();
 
-            	var firstTodoText = $('.todo-items').find($('.todo-description')).text();
+            	var firstTodoText = $('.todo-item').first().children('.todo-description').text();
+            	// var firstTodoText = 'Grab some beer.';
 
-            	expect(firstTodoText).to.contain('Get a ride to airport.');
-            	
+            	expect(firstTodoText).to.contain('Grab some beer.');
+            	// expect(true).to.equal(true);
+
             });
+
+            it('should do something else...');
         });
     });
 })();
