3645d3730f5ca9ee94ece3decfdbb2ed395a3f48
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-02-13 00:27:09 -0500

    Added remove function. Started working on edit feature. Added remove test.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,53 +1,118 @@
 console.log('You can do this!');
 
 var todoList = [
-{
-	description: 'Pack for Texas',
-	done: false,
-	id: _.uniqueId('todo')
-}, 
+	{
+		description: 'Pack for Texas',
+		done: false,
+		id: _.uniqueId('todo')
+	},
 
-{
-	description: 'Drink some wine',
-	done: false,
-	id: _.uniqueId('todo')
-}, 
+	{
+		description: 'Drink some wine',
+		done: false,
+		id: _.uniqueId('todo')
+	},
 
-{
-	description: 'Get a ride to the airport',
-	done: false,
-	id: _.uniqueId('todo')
-}
+	{
+		description: 'Get a ride to the airport',
+		done: false,
+		id: _.uniqueId('todo')
+	}
 
 ];
 
-// Template from index
-var todoTemplate = _.template($('.todo-template').text())
+$(document).ready(function(){
 
-_.each(todoList, function(item){
-	$('.todo-items').prepend(todoTemplate(item));
-})
+	// Template from index
+	var todoTemplate = _.template($('.todo-template').text());
+	var todoEditTemplate = _.template($('.todoEditTemplate').text());
 
-$('.js-add-todo').click(function(){
+	_.each(todoList, function(item){
+		$('.todo-items').prepend(todoTemplate(item));
+	});
 
-// pull the data from the user's input
-	var description = $('.js-new-todo-input').val();
+	$('.todo-item').hover(function(){
+		$(this).children('.edit-button').toggleClass('visible')
+	});
 
-// push the user's input into an object
-var todo = {
-	description: description,
-	done: false,
-	id: _.uniqueId('todo')
-}
+		$('.todo-item').hover(function(){
+		$(this).children('.remove-button').toggleClass('visible')
+	});
 
-// Push the user's data into a template using our template function
-var renderedTemplate = todoTemplate(todo);
 
-$('.todo-items').prepend(renderedTemplate);
+	$('.js-add-todo').click(function(){
 
-(todoList).push(todo)
+		// pull the data from the user's input
+		var description = $('.js-new-todo-input').val();
 
-// $('.todo-input-box').trigger("reset");  
-// Not resetting - do I need to add a form tag for reset to work properly?
+		// push the user's input into an object
+		var todo = {
+			description: description,
+			done: false,
+			id: _.uniqueId('todo')
+		};
+
+		// Push the user's data into a template using our template function
+		var renderedTemplate = todoTemplate(todo);
+
+		$('.todo-items').prepend(renderedTemplate);
+
+		(todoList).push(todo);
+
+		// $('.todo-input-box').trigger("reset");  
+		// Not resetting - do I need to add a form tag for reset to work properly?
+
+	});
+
+
+
+	$('.todo-items').on('click', '.js-remove-button', function(){
+		console.log('About to take this sucka out.');
+		var parentId = $(this).parent().attr('id');
+		console.log('About to remove ',parentId);
+		console.log('Array length before removal is ', todoList.length);
+		todoList = _.reject(todoList, function(item){return item.id == parentId;});
+		console.log('Array length after removal is', todoList.length);
+		$(this).parent().remove();
+		console.log('Removed ',$(this).parent())
+	});
+
+	$('.todo-items').on('click', '.js-done-button', function(){
+		console.log('About to make this done.');
+		var parentId = $(this).parent().attr('id');
+		console.log('Grabbing parent ID:', parentId)
+		todoArrayItem = _.findWhere(todoList, function(item){return item.id == parentId});
+		newDoneStatus = !todoArrayItem.done;
+		todoArrayItem.done = newDoneStatus;
+		console.log('Done status changed for ', parentId);
+		$(this).toggleClass('done');
+		console.log('Changed the done-button.');
+		$(this).siblings('.todo-description').toggleClass('done');
+		console.log('Changed the todo-description.');
+	});
+
+	$('.todo-items').on('click', '.js-edit-button', function(){
+		$(this).children('.todo-description').replaceAll(editTemplate);
+		console.log('Input should replace .description');
+
+	});
+
+$('.todo-items').on('click', '.js-update-button', function(){
+		var parentId = $(this).parent().attr('id');
+		console.log('About to update', parentId);
+
+		todoArrayItem = _.findWhere(todoList, function(item){return item.id == parentId});
+
+		var editedTemplate = todoEditTemplate(todo);
+
+		var newDescription = $('.edit-input').val();
+
+		todoArrayItem.description = newDescription;
+		console.log('New description should be put into array.');
+		$(this).children('.todo-description').replaceAll(editTemplate);
+		console.log('Input should replace .description');
+
+	});
+
 
 });
--- test/spec/test.js
+++ test/spec/test.js
@@ -6,20 +6,28 @@
     describe('In our todo app', function () {
         describe('when the add button is clicked', function () {
             it('should add a new todo div when the input is populated', function () {
-            	
             	$('.js-new-todo-input').val('Grab some beer.');
-
             	$('.js-add-todo').click();
-
             	var firstTodoText = $('.todo-item').first().children('.todo-description').text();
             	// var firstTodoText = 'Grab some beer.';
-
             	expect(firstTodoText).to.contain('Grab some beer.');
             	// expect(true).to.equal(true);
-
             });
 
-            it('should do something else...');
+            it('should increase the array size by 1', function(){
+            	$('.js-new-todo-input').val('Grab some beer.');
+            	$('.js-add-todo').click();
+            	var firstTodoText = $('.todo-item').first().children('.todo-description').text();
+            	expect(todoList.length).to.equal(4);
+            });
+        });
+        
+        describe('when the remove button is clicked', function () {
+        		it('should remove the corresponding parent div', function(){
+        			// ($('#todo1').children($('.remove-button'))).click();
+        		});
+        		it('should remove the corresponding object from the array');
+
         });
     });
 })();
